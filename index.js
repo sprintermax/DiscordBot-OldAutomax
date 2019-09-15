cb1d3772dd85f9934f9ceee49debee1a85028451
Sprintermax <ericfnoe7@hotmail.com>
2019-09-15 17:02:04 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,28 +10,44 @@ const bot = new comando.Client({
 });
 
 const prefix = "s!";
+var peixotos = [
+    'https://cdn.discordapp.com/attachments/583495087200141324/622875540243611688/peixoto_3.png',
+    'https://cdn.discordapp.com/attachments/583495087200141324/622875537345347584/2019-07-21_00_35_05-Window.png',
+    'https://cdn.discordapp.com/attachments/583495087200141324/622875541912944642/2019-07-20_15_28_55-Window.png',
+    'https://cdn.discordapp.com/attachments/583495087200141324/622877576012234753/unknown.png',
+    'https://cdn.discordapp.com/attachments/583491033363513345/622877737996255244/images.png',
+    'https://cdn.discordapp.com/attachments/583491033363513345/622877947077984266/images.png',
+    'https://cdn.discordapp.com/attachments/583491033363513345/622878137830866944/Z.png',
+    'https://cdn.discordapp.com/attachments/583491033363513345/622878758306840586/unknown.png',
+    'https://cdn.discordapp.com/attachments/591077338687471618/622878907829321741/maxresdefault.png',
+    'https://cdn.discordapp.com/attachments/591077338687471618/622879480368857089/62262187_119947442575487_6031054289039767912_n.png',
+    'https://cdn.discordapp.com/attachments/583495087200141324/622882527287443489/a7ddef3a23236fd6a7eab388a6664112e57a0376r1-1920-1080v2_00.png',
+    'https://cdn.discordapp.com/attachments/583495087200141324/622882638285504522/Fishstick.png',
+    'https://cdn.discordapp.com/attachments/583495087200141324/622882878820450316/68689104_101374137872999_1973897991852419550_n.png'
+];
+
 var MinecraftDeaths = [
-    ' foi esmagado(a) por uma bigorna em queda',
-    ' foi espetado(a) até a morte',
-    ' morreu afogado(a)',
-    ' explodiu',
-    ' caiu no chão com muita força',
-    ' foi esmagado(a) por um bloco em queda',
-    ' explodiu com um estrondo',
-    ' experimentou energia cinética',
-    ' morreu',
-    ' pegou fogo',
-    ' sufocou-se em uma parede',
-    ' tentou nadar na lava',
-    ' foi atingido(a) por um raio',
-    ' foi morto(a) por magia',
-    ' descobriu que o chão era de lava',
-    ' queimou até a morte',
-    ' caiu para fora do mundo',
-    ' caiu de um lugar alto',
-    ' caiu de uma escada',
-    ' caiu de algumas vinhas',
-    ' foi condenado(a) a cair'
+    'foi esmagado(a) por uma bigorna em queda',
+    'foi espetado(a) até a morte',
+    'morreu afogado(a)',
+    'explodiu',
+    'caiu no chão com muita força',
+    'foi esmagado(a) por um bloco em queda',
+    'explodiu com um estrondo',
+    'experimentou energia cinética',
+    'morreu',
+    'pegou fogo',
+    'sufocou-se em uma parede',
+    'tentou nadar na lava',
+    'foi atingido(a) por um raio',
+    'foi morto(a) por magia',
+    'descobriu que o chão era de lava',
+    'queimou até a morte',
+    'caiu para fora do mundo',
+    'caiu de um lugar alto',
+    'caiu de uma escada',
+    'caiu de algumas vinhas',
+    'foi condenado(a) a cair'
 ];
 
 /*bot.on('ready', () => {
@@ -57,6 +73,15 @@ bot.on("message", (message) => {
             message.channel.send(`${message.author} abraçou ${user}!`);
         }
     } else
+    if (command === "peixoto") {
+        var peixoto = peixotos[Math.floor(Math.random() * peixotos.length)];
+        if (args.length < 1){
+            message.channel.send(`${message.author} aqui está o Peixoto:`, new discord.Attachment(peixoto, 'peixoto.png'));
+        } else {
+            let [user] = args;
+            message.channel.send(`${message.author} mostrou o Peixoto para ${user}!`, new discord.Attachment(peixoto, 'peixoto.png'));
+        }
+    } else
     if (command === "dado") {
         var dicevalue = Math.floor(Math.random() * 6 ) + 1;
         message.channel.send(`${message.author} Jogou um dado e obteve o número ${dicevalue}`);
@@ -67,7 +92,7 @@ bot.on("message", (message) => {
         } else {
             let [user] = args;
             var killmessage = MinecraftDeaths[Math.floor(Math.random() * MinecraftDeaths.length)];
-            message.channel.send(`${user}${killmessage}`);
+            message.channel.send(`${user} ${killmessage}`);
         }
     } else
     if (command === "give") {
@@ -84,7 +109,7 @@ bot.on("message", (message) => {
         }
     } else
     if (command === "fn") {
-if (args.length < 1){
+        if (args.length < 1){
             message.channel.send(`${message.author}\ns!fn <Username> <pc, xbl, psn> <global, solo, duo, squad>`);
         } else {
             let [user, platform, gamemode] = args;
@@ -97,6 +122,7 @@ if (args.length < 1){
                     return;
                 }
             } else {
+                args[1] = `pc`;
                 var plataforma = `pc`;
             }
             if (args[2]) {
@@ -108,6 +134,7 @@ if (args.length < 1){
                     return;
                 }
             } else {
+                args[2] = `global`;
                 var gametype = `global`;
             }
 
@@ -130,16 +157,20 @@ if (args.length < 1){
                 let wins = modestats.wins;
                 let kills = modestats.kills;
                 let kd = modestats.kd;
+                
+                var msg = "";
+                msg += "\nPartidas Jogadas: " + matches;
+                msg += "\nEliminações: " + kills;
+                msg += "\nVitórias: " + wins;
+                msg += "\nK/D: " + kd;
 
-                let embed = new discord.RichEmbed()
-                .setAuthor(`Mostrando estatística ${gametype} de ${data.username} na plataforma ${plataforma}`)
+                var embed = new discord.RichEmbed()
+                .setAuthor(data.username)
+                .setDescription(msg)
                 .setColor("#FF00FF")
-                .addField("Vitórias", wins, true)
-                .addField("Eliminações", kills, true)
-                .addField("Partidas Jogadas", matches, true)
-                .addField("K/D", kd, true);
+                .setThumbnail("https://pbs.twimg.com/profile_images/1101123838589452299/rpu5UKTH.png");
 
-                return message.channel.send(`${message.author} Aqui está:`, embed);
+                return message.channel.send(`${message.author} Mostrando estatística ${gametype} de ${data.username} na plataforma ${plataforma}`, embed);
             })
         }
     }
