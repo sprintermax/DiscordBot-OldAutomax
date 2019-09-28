4c9d6d5914ffc86ad4169c1e27a9fdfb7a148feb
Sprintermax <ericfnoe7@hotmail.com>
2019-09-28 15:13:49 -0300

    Add files via upload

--- src/comandos/say.js
+++ src/comandos/say.js
@@ -1,9 +1,7 @@
 const config = require("../config.json");
 
-const allowedroles = config.serverinfo.allowedtosay;
-
 module.exports.run = async (message, prefix) => {
-    if(message.member.roles.some(r=>allowedroles.includes(r.id)) ) {
+    if(message.member.hasPermission("MANAGE_MESSAGES")) {
         message.channel.send(message.content.slice(prefix.length + 3).trim());
       } else {
         message.channel.send(`${message.author}\nVocê não tem permissão para usar esse comando!`);
