4f6b3ecb117e0811f79382966a399769bd1ec7c4
Sprintermax <ericfnoe7@hotmail.com>
2019-11-21 23:01:32 -0300

    removed server command

--- src/comandos/help.js
+++ src/comandos/help.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args, prefix) => {
     msg += '\n\`hug <usuário>\`\n└Abraça o Usuário mencionado';
     msg += '\n\`peixoto <opcional:usuário>\`\n└Mostra uma imagem aleatória do Peixoto a você ou ao Usuário mencionado';
     msg += '\n\`kill <usuário>\`\n└Manda uma mensagem de morte do Minecraft para o Usuário mencionado';
-    msg += '\n\`paraiso <opcional:usuário>\`\n└Envia o link de convite do servidor "Paraiso do Sprinter" para você ou para o Usuário mencionado';
     msg += '\n\`give <usuário> <item> <opcional:quantidade>\`\n└Dá um item simulando um comando do Minecraft para o Usuário mencionado';
     msg += '\n\`roll <opcional:quantidade>\`\n└Escolhe um número aleatório entre entre 1 até o valor escolhido (Padrão: 1 a 6)';
     msg += '\n\`say <texto>\`\n└Faz com que o Bot fale algo no chat como se fosse você';
